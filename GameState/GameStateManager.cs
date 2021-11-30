e8e66a8ab6fcdf92482d957c2ea67e1ac718c98b
PickDown-py <sodapumpkin@icloud.com>
2021-11-30 21:20:01 +0200

    fix missing scenes

--- GameState/GameStateManager.cs
+++ GameState/GameStateManager.cs
@@ -6,37 +6,37 @@ using HunterGame.Animals.Population;
 
 namespace HunterGame.GameState
 {
-    public class GameStateManager: Node
-    {
-        private Population _population;
-        
-        public override void _Ready()
-        {
-            _population = GetNode<Population>("/root/Population");
-        }
+	public class GameStateManager: Node
+	{
+		private Population _population;
+		
+		public override void _Ready()
+		{
+			_population = GetNode<Population>("/root/Population");
+		}
 
-        public void KillAnimal(Animal animal)
-        {
-             switch (animal)
-            {
-                case Predator pred:
-                    _population.RemovePredator(pred);
-                    break;
-                case Prey prey: 
-                    _population.RemovePrey(prey);
-                    break;
-            }
-             
-            animal.QueueFree();
-        }
+		public void KillAnimal(Animal animal)
+		{
+			 switch (animal)
+			{
+				case Predator pred:
+					_population.RemovePredator(pred);
+					break;
+				case Prey prey: 
+					_population.RemovePrey(prey);
+					break;
+			}
+			 
+			animal.QueueFree();
+		}
 
-        public void KillHunter()
-        {
-            new List<Predator>(_population.Predators).ForEach(_population.RemovePredator);
-            new List<Prey>(_population.Preys).ForEach(p => _population.RemovePrey(p));
+		public void KillHunter()
+		{
+			new List<Predator>(_population.Predators).ForEach(_population.RemovePredator);
+			new List<Prey>(_population.Preys).ForEach(p => _population.RemovePrey(p));
 
-            GetTree().ReloadCurrentScene();
-        }
-        
-    }
+			GetTree().ReloadCurrentScene();
+		}
+		
+	}
 }
