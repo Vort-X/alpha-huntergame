b6863120eb67559b343bafd27b7cbe007af471e9
lavorMax <lavor.maxim@gmail.com>
2021-12-03 19:01:13 +0200

    Add prey\predator behaviours

---
+++ Behaviours/ChasePray.cs
@@ -0,0 +1,47 @@
+﻿using Godot;
+using HunterGame.Animals;
+using HunterGame.Animals.Population;
+using HunterGame.Movement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterGame.Behaviours
+{
+    public class ChasePray : Node, IBehaviour
+    {
+        private List<Prey> preys;
+
+        public override void _Ready()
+        {
+            preys = GetNode<Population>("Population").Preys;
+        }
+
+        public Vector2 Target(Vector2 position, Vector2 direction)
+        {
+            Prey closestPrey = ClosestVisiblePray(position);
+            return closestPrey != null ? closestPrey.GlobalPosition - position : direction;
+        }
+
+        private Prey ClosestVisiblePray(Vector2 position)
+        {
+            Prey closest = null;
+            double closestDistance = 0;
+            foreach (Prey p in preys)
+            {
+                double distanceToPrey = Math.Sqrt(Math.Pow(p.GlobalPosition.x - position.x, 2) + Math.Pow(p.GlobalPosition.y - position.y, 2));
+                if (closest == null
+                    || (distanceToPrey < 110
+                    && closestDistance > distanceToPrey))
+                {
+                    closest = p;
+                    closestDistance = distanceToPrey;
+                }
+            }
+
+            return closest;
+        }
+    }
+}
---
+++ Behaviours/RunFromEnemy.cs
@@ -0,0 +1,66 @@
+﻿using Godot;
+using HunterGame.Animals;
+using HunterGame.Animals.Population;
+using HunterGame.Movement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterGame.Behaviours
+{
+
+    public class RunFromEnemy : Node, IBehaviour
+    {
+        private List<Predator> predators;
+        private Hunter hunter;
+
+        public override void _Ready()
+        {
+            predators = GetNode<Population>("Population").Predators;
+            hunter = GetNode<Hunter>("Hunter");
+        }
+
+        public Vector2 Target(Vector2 position, Vector2 direction)
+        {
+            Vector2 runVector = VectorToClosestEnemy(position);
+            return runVector == Vector2.Zero ? direction :  position - runVector;
+        }
+
+        private Vector2 VectorToClosestEnemy(Vector2 position)
+        {
+            (Predator, double) closestPredator = ClosestVisiblePredatorWithDistance(position);
+            double distanceToHunter = Math.Sqrt(Math.Pow(hunter.GlobalPosition.x - position.x, 2) + Math.Pow(hunter.GlobalPosition.y - position.y, 2));
+            if (closestPredator.Item1 == null
+                    || (distanceToHunter < 100
+                    && closestPredator.Item2 > distanceToHunter))
+            {
+                return hunter.GlobalPosition;
+            }
+
+            return closestPredator.Item1 != null ? closestPredator.Item1.GlobalPosition : Vector2.Zero;
+        }
+
+        private (Predator, double) ClosestVisiblePredatorWithDistance(Vector2 position)
+        {
+            Predator closest = null;
+            double closestDistance = 0;
+            foreach (Predator p in predators)
+            {
+                double distanceToPredator = Math.Sqrt(Math.Pow(p.GlobalPosition.x - position.x, 2) + Math.Pow(p.GlobalPosition.y - position.y, 2));
+                if (closest == null
+                    || (distanceToPredator < 100
+                    && closestDistance > distanceToPredator))
+                {
+                    closest = p;
+                    closestDistance = distanceToPredator;
+                }
+            }
+
+            return (closest, closestDistance);
+        }
+
+        
+}
+}
